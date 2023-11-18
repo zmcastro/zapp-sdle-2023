c7378f014a9c0db5a07717dc54ebc354b96a943b
toni-santos <a.oliveirasantos122@gmail.com>
2023-11-18 15:20:42 +0000

    implement g-counter

---
+++ local/src/lib/g-counter.js
@@ -0,0 +1,43 @@
+export class GCounter {
+
+    #map = new Map();
+    #id;
+
+    constructor(id) {
+        this.#id = id;
+        this.#map[id] = 0;
+    }
+
+    // Get map
+    get() {
+        const res = this.#map;
+        return res;
+    }
+
+    // Read local counter value
+    local() {
+        return this.#map.get(this.#id);
+    }
+    
+    // Read counter value
+    read() {
+        let res = 0;
+        for (const [id, value] of Object.entries(this.#map)) {
+            res += value;
+        }
+        return res;
+    }
+
+    // Increment value
+    inc(tosum=1) {
+        this.#map[this.#id] += tosum;
+    }
+
+    // Merge counters
+    join(gcounter) {
+        for (const [id, value] of Object.entries(gcounter.get())) {
+            if (!this.#map.has(id)) this.#map[id] = 0;
+            this.#map[id] = Math.max(this.#map[id], value);
+        }
+    }
+}
--- local/src/routes/api/test/+server.js
+++ local/src/routes/api/test/+server.js
@@ -1,5 +1,23 @@
 import { json } from '@sveltejs/kit';
+import { GCounter } from "$lib/g-counter.js";
 
 export async function POST({ request }) {
+	const x = new GCounter("x");
+	const y = new GCounter("y");
+	const z = new GCounter("z");
+
+	x.inc(); x.inc();
+	y.inc(2);
+	z.join(x); z.join(y);
+	
+	console.log("should be 4: ", z.read()); // 4
+	
+	x.inc(2);
+	z.inc(2);
+	z.join(x);
+	z.join(x);
+  
+	console.log("should be 8: ", z.read()); // 8
+	console.log(z.get());
 	return json("hi");
 }
