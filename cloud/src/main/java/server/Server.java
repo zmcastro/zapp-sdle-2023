be8713c4641d1fb68426216ffe9418e7f746f4cd
pedronunes19 <pedromiguelmnunes@gmail.com>
2023-12-07 20:34:14 +0000

    fix

--- cloud/src/main/java/server/Server.java
+++ cloud/src/main/java/server/Server.java
@@ -68,29 +68,4 @@ public class Server {
             }
         };
     }
-
-    @Bean
-    public ConsistentHashing consistentHashing() {
-        return new ConsistentHashing(5);
-    }
-
-    @Bean
-    public DBHandler dbHandler() {
-        return new DBHandler();
-    }
-
-    @Bean
-    public ApplicationRunner init(ConsistentHashing consistentHashing, DBHandler dbHandler) {
-        return new ApplicationRunner() {
-            @Override
-            public void run(ApplicationArguments args) throws Exception {
-                // Your initialization code here
-                consistentHashing.addNode("db_1");
-                consistentHashing.addNode("db_2");
-                consistentHashing.addNode("db_3");
-                consistentHashing.addNode("db_4");
-                consistentHashing.addNode("db_5");
-            }
-        };
-    }
 }
